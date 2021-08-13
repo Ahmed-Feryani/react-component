30b113426fb2f2bd7eb6fd74845bedb146d91f8e
feriany_ahmed <ahmed.feryani.dev@gmail.com>
2021-08-13 09:27:58 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <div>Adress:</div>
+      <div>hay chabab gafsa</div>
+    </div>
+  );
+};
+
+export default Address;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FullName = () => {
+  return (
+    <div>
+      <div>Name:</div>
+      <div>Feryani Ahmed</div>
+    </div>
+  );
+};
+
+export default FullName;
